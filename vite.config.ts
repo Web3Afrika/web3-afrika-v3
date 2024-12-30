811d64b4b4306d95a414371adb5c81304a479b83
Ayoola Abdulqudus <qqudusayo@gmail.com>
2024-12-30 02:40:47 +0100

    setup discord redirect url

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-	plugins: [react()],
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	return {
+		plugins: [react()],
+		server: {
+			proxy: {
+				"/discord": {
+					target: env.VITE_DISCORD_URL,
+					changeOrigin: true,
+					rewrite: path => path.replace(/^\/discord/, ""),
+				},
+			},
+		},
+	};
 });
