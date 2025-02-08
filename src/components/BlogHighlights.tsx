d5103fb322a564156046fed5566366c6acf1e977
Ayoola Abdulqudus <qqudusayo@gmail.com>
2025-02-08 21:59:40 +0100

    remove deprecated articles data from constants

--- src/components/BlogHighlights.tsx
+++ src/components/BlogHighlights.tsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { articles } from "../constants";
 import { cn, formatDate } from "../util";
 import { AnimatedText } from "./animated-text";
 import { FadeIn, FadeInStagger } from "./FadeIn";
 import { ChevronRight } from "./icons";
+
+type Article = {
+	id: string;
+	title: string;
+	link: string;
+	date: string;
+	content: string;
+	author: string;
+	imageUrl: string;
+};
 
 const BlogHighlights = ({
 	noDescription,
@@ -12,6 +22,49 @@ const BlogHighlights = ({
 	noDescription?: boolean;
 	partial?: boolean;
 }) => {
+	const [articles, setArticles] = useState<Article[]>([]);
+
+	useEffect(() => {
+		console.log("Sent a fetch request to the RSS feed");
+		async function fetchRSS() {
+			try {
+				const response = await fetch(
+					`https://api.allorigins.win/get?url=${encodeURIComponent("https://blog.web3afrika.com/rss.xml")}`,
+				);
+
+				if (!response.ok) {
+					throw new Error(`HTTP error! Status: ${response.status}`);
+				}
+
+				const data = await response.json();
+				const xmlText = data.contents;
+				const parser = new DOMParser();
+				const xmlDoc = parser.parseFromString(xmlText, "text/xml");
+
+				const filteredItems = [...xmlDoc.querySelectorAll("item")].map(
+					item => ({
+						id: item.querySelector("guid")?.textContent || "",
+						title: item.querySelector("title")?.textContent || "",
+						link: item.querySelector("link")?.textContent || "",
+						date: item.querySelector("pubDate")?.textContent || "",
+						content: item.querySelector("description")?.textContent || "",
+						author:
+							item.getElementsByTagName("dc:creator")[0]?.textContent || "",
+						imageUrl:
+							item.getElementsByTagName("hashnode:coverImage")[0]
+								?.textContent || "",
+					}),
+				);
+
+				setArticles(filteredItems);
+			} catch (error) {
+				console.error("Error fetching RSS feed:", error);
+			}
+		}
+
+		fetchRSS();
+	}, []);
+
 	return (
 		<section className="container mx-auto w-11/12 py-8">
 			{!noDescription && (
@@ -35,107 +88,111 @@ const BlogHighlights = ({
 				</div>
 			)}
 
-			<div className="grid gap-6 md:grid-cols-3">
-				<FadeIn className="col-span-full flex flex-col-reverse bg-[#FAFAFA] p-6 dark:bg-[#121212] md:grid md:grid-cols-9 md:gap-8 md:rounded-lg md:p-8">
-					<div className="col-span-4">
-						<div className="flex h-full flex-col justify-center overflow-hidden rounded-lg">
-							<div className="my-4 md:my-auto md:px-6">
-								<span className="text-lg text-[#9E9E9E]">
-									{formatDate(articles[0].date)}
-								</span>
-							</div>
-
-							<div className="max-w-md space-y-6 md:p-6">
-								<h2 className="text-base text-[#5D5D5D] dark:text-white md:text-3xl">
-									{articles[0].title}
-								</h2>
-
-								<p className="line-clamp-3 text-xs text-[#9E9E9E] md:text-xl">
-									{articles[0].content}
-								</p>
-
-								<span className="block text-sm text-[#5D5D5D] md:text-base">
-									{articles[0].author}
-								</span>
-
-								<a
-									target="_blank"
-									rel="noreferrer"
-									href={articles[0].link}
-									className="inline-block rounded-full border border-[#0C6E5F] px-6 py-2 text-sm text-[#0C6E5F] shadow-lg shadow-[#469F9214] transition-colors duration-200 hover:bg-[#0C6E5F] hover:text-white"
-								>
-									Read Article
-								</a>
-							</div>
-						</div>
-					</div>
-
-					<div className="col-span-5 flex items-center justify-center overflow-hidden rounded">
-						<img
-							src={articles[0].imageUrl}
-							alt="Featured"
-							className="max-w-full object-cover"
-						/>
-					</div>
-				</FadeIn>
-				<FadeInStagger className="col-span-full grid gap-6 md:grid-cols-3">
-					{(partial ? articles.slice(1, 4) : articles.slice(1)).map(article => (
-						<FadeIn
-							key={article.id}
-							className="flex flex-col overflow-hidden bg-[#FAFAFA] p-6 dark:bg-[#121212] md:rounded-lg md:p-8"
-						>
-							<div
-								className={
-									cn("box-border block", "relative w-full")
-									// article.bg,
-								}
-								style={{
-									width: "initial",
-									height: "initial",
-									padding: "52.5% 0 0",
-								}}
-							>
-								<img
-									src={article.imageUrl}
-									alt={article.title}
-									className="absolute left-0 top-0 h-full w-full rounded-lg object-cover md:rounded"
-								/>
-							</div>
-
-							<div className="my-4">
-								<span className="text-lg text-[#9E9E9E]">
-									{formatDate(article.date)}
-								</span>
-							</div>
-
-							<div className="flex h-full flex-col">
-								<div className="flex-grow">
-									<h2 className="mb-4 text-[#5D5D5D] dark:text-white md:text-2xl">
-										{article.title}
-									</h2>
-
-									<p className="mb-4 text-xs text-[#9E9E9E] md:text-sm">
-										{article.content}
-									</p>
-
-									<span className="mb-4 block text-sm text-[#5D5D5D] md:text-base">
-										{article.author}
+			{!!articles.length && (
+				<div className="grid gap-6 md:grid-cols-3">
+					<FadeIn className="col-span-full flex flex-col-reverse bg-[#FAFAFA] p-6 dark:bg-[#121212] md:grid md:grid-cols-9 md:gap-8 md:rounded-lg md:p-8">
+						<div className="col-span-4">
+							<div className="flex h-full flex-col justify-center overflow-hidden rounded-lg">
+								<div className="my-4 md:my-auto md:px-6">
+									<span className="text-lg text-[#9E9E9E]">
+										{formatDate(articles[0].date)}
 									</span>
 								</div>
 
-								<a
-									target="_blank"
-									rel="noreferrer"
-									href={article.link}
-									className="inline-block w-fit rounded-full border border-[#0C6E5F] px-6 py-2 text-sm text-[#0C6E5F] shadow-lg shadow-[#469F9214] transition-colors duration-200 hover:bg-[#0C6E5F] hover:text-white"
-								>
-									Read Article
-								</a>
+								<div className="max-w-md space-y-6 md:p-6">
+									<h2 className="text-base text-[#5D5D5D] dark:text-white md:text-3xl">
+										{articles[0].title}
+									</h2>
+
+									<p className="line-clamp-3 text-xs text-[#9E9E9E] md:text-xl">
+										{articles[0].content}
+									</p>
+
+									<span className="block text-sm text-[#5D5D5D] md:text-base">
+										{articles[0].author}
+									</span>
+
+									<a
+										target="_blank"
+										rel="noreferrer"
+										href={articles[0].link}
+										className="inline-block rounded-full border border-[#0C6E5F] px-6 py-2 text-sm text-[#0C6E5F] shadow-lg shadow-[#469F9214] transition-colors duration-200 hover:bg-[#0C6E5F] hover:text-white"
+									>
+										Read Article
+									</a>
+								</div>
 							</div>
-						</FadeIn>
-					))}
-				</FadeInStagger>
-			</div>
+						</div>
+
+						<div className="col-span-5 flex items-center justify-center overflow-hidden rounded">
+							<img
+								src={articles[0].imageUrl}
+								alt="Featured"
+								className="max-w-full object-cover"
+							/>
+						</div>
+					</FadeIn>
+					<FadeInStagger className="col-span-full grid gap-6 md:grid-cols-3">
+						{(partial ? articles.slice(1, 4) : articles.slice(1)).map(
+							article => (
+								<FadeIn
+									key={article.id}
+									className="flex flex-col overflow-hidden bg-[#FAFAFA] p-6 dark:bg-[#121212] md:rounded-lg md:p-8"
+								>
+									<div
+										className={
+											cn("box-border block", "relative w-full")
+											// article.bg,
+										}
+										style={{
+											width: "initial",
+											height: "initial",
+											padding: "52.5% 0 0",
+										}}
+									>
+										<img
+											src={article.imageUrl}
+											alt={article.title}
+											className="absolute left-0 top-0 h-full w-full rounded-lg object-cover md:rounded"
+										/>
+									</div>
+
+									<div className="my-4">
+										<span className="text-lg text-[#9E9E9E]">
+											{formatDate(article.date)}
+										</span>
+									</div>
+
+									<div className="flex h-full flex-col">
+										<div className="flex-grow">
+											<h2 className="mb-4 text-[#5D5D5D] dark:text-white md:text-2xl">
+												{article.title}
+											</h2>
+
+											<p className="mb-4 text-xs text-[#9E9E9E] md:text-sm">
+												{article.content}
+											</p>
+
+											<span className="mb-4 block text-sm text-[#5D5D5D] md:text-base">
+												{article.author}
+											</span>
+										</div>
+
+										<a
+											target="_blank"
+											rel="noreferrer"
+											href={article.link}
+											className="inline-block w-fit rounded-full border border-[#0C6E5F] px-6 py-2 text-sm text-[#0C6E5F] shadow-lg shadow-[#469F9214] transition-colors duration-200 hover:bg-[#0C6E5F] hover:text-white"
+										>
+											Read Article
+										</a>
+									</div>
+								</FadeIn>
+							),
+						)}
+					</FadeInStagger>
+				</div>
+			)}
 		</section>
 	);
 };
