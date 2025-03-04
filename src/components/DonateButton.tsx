16a2a520941512546756f8f859057522699a1ceb
Ayoola Abdulqudus <qqudusayo@gmail.com>
2025-03-04 22:45:25 +0100

    feat: update donate button

--- src/components/DonateButton.tsx
+++ src/components/DonateButton.tsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { cn } from "../util";
-import { CloseSquare, Logo } from "./icons";
+import { CloseSquare, Copy, Logo } from "./icons";
+
+import Scroll from "../assets/brands/ scroll.svg";
+import Arbitrum from "../assets/brands/arbitrum.svg";
+import EthLogo from "../assets/brands/ethereum.png";
+import Optimism from "../assets/brands/optimism.svg";
 
 const DonateButton = ({ isText }: { isText?: boolean }) => {
 	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	const modalOpenHandler = (state: boolean) => {
-		console.log(state);
 		setIsModalOpen(state);
 	};
 
@@ -42,20 +46,92 @@ const Modal = ({
 	className?: string;
 	modalOpenHandler?: () => void;
 }) => {
+	const [isAddressCopied, setIsAddressCopied] = useState(false);
+
+	const copyAddress = (e: React.MouseEvent<HTMLButtonElement>) => {
+		e.preventDefault();
+		e.stopPropagation();
+		navigator.clipboard.writeText("0x4BaF3334dF86FB791A6DF6Cf4210C685ab6A1766");
+		setIsAddressCopied(true);
+		setTimeout(() => {
+			setIsAddressCopied(false);
+		}, 3000);
+	};
+
 	return (
-		<div
-			className={cn(
-				"fixed inset-0 top-0 z-50 !m-0 bg-black bg-opacity-0 transition-opacity duration-200",
-				className,
-			)}
-			onClick={modalOpenHandler}
-		>
-			<div className="relative top-24 z-50 mx-auto w-11/12 max-w-4xl space-y-16 overflow-hidden rounded-2xl bg-white p-6 shadow-lg dark:bg-[#121212]">
+		<div className={cn("fixed inset-0 top-0 z-20", className)}>
+			<div
+				className={cn(
+					"fixed inset-0 top-0 z-20 !m-0 bg-black bg-opacity-0 transition-opacity duration-200",
+					className,
+				)}
+				onClick={modalOpenHandler}
+			></div>
+			<div className="relative top-24 z-50 mx-auto w-11/12 max-w-4xl space-y-6 overflow-hidden rounded-2xl bg-white p-6 shadow-lg dark:bg-[#121212]">
 				<div className="flex items-center justify-between">
 					<Logo className="fill-black dark:fill-white" />
 					<CloseSquare className="cursor-pointer" onClick={modalOpenHandler} />
 				</div>
-				<div>Waiting for addresses to be added...</div>
+				<div className="rounded-lg border border-gray-200 px-4 py-6">
+					<div>
+						<h2>Donation</h2>
+						<p className="text-gray-400">
+							Donations are to be made to our Ethereum address.
+						</p>
+					</div>
+
+					<h2 className="mt-6">Supported Networks</h2>
+					<div className="my-4 mb-2 flex space-x-4">
+						<div className="rounded-full border p-2">
+							<img
+								src={EthLogo}
+								alt="Ethereum"
+								className="size-8 rounded-full"
+							/>
+						</div>
+						<div className="rounded-full border p-2">
+							<img src={Scroll} alt="Scroll" className="size-8 rounded-full" />
+						</div>
+						<div className="rounded-full border p-2">
+							<img
+								src={Optimism}
+								alt="Optimism"
+								className="size-8 rounded-full"
+							/>
+						</div>
+						<div className="rounded-full border p-2">
+							<img
+								src={Arbitrum}
+								alt="Arbitrum"
+								className="size-8 rounded-full"
+							/>
+						</div>
+					</div>
+					<span
+						className={cn(
+							"text-sm transition duration-300 dark:text-white",
+							isAddressCopied ? "opacity-100" : "opacity-0",
+						)}
+					>
+						Address copied to clipboard.
+					</span>
+					<div className="flex flex-col gap-2 sm:flex-row">
+						<input
+							type="text"
+							placeholder="0x4BaF3334dF86FB791A6DF6Cf4210C685ab6A1766"
+							value="0x4BaF3334dF86FB791A6DF6Cf4210C685ab6A1766"
+							className="flex-1 rounded-lg border border-gray-200 p-2 text-gray-400"
+							disabled
+						/>
+						<button
+							className="flex w-full items-center justify-center gap-4 rounded-lg border border-gray-200 px-8 py-2 text-gray-400 transition duration-300 dark:bg-black md:w-fit"
+							onClick={copyAddress}
+						>
+							<Copy />
+							<span>Copy</span>
+						</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,7 +12,6 @@ const Navbar = () => {
 	const location = useLocation();
 
 	const modalOpenHandler = (state: boolean) => {
-		console.log(state);
 		setIsModalOpen(state);
 	};
 
@@ -74,13 +73,14 @@ const Modal = ({
 	const location = useLocation();
 
 	return (
-		<div
-			className={cn(
-				"fixed inset-0 z-50 bg-black bg-opacity-0 transition-opacity duration-200",
-				className,
-			)}
-			onClick={modalOpenHandler}
-		>
+		<div className={cn("fixed inset-0 top-0 z-20", className)}>
+			<div
+				className={cn(
+					"fixed inset-0 top-0 z-20 !m-0 bg-black bg-opacity-0 transition-opacity duration-200",
+					className,
+				)}
+				onClick={modalOpenHandler}
+			></div>
 			<div className="container relative top-24 z-50 mx-auto w-11/12 overflow-hidden rounded-2xl bg-white p-6 shadow-lg dark:bg-[#121212]">
 				<div className="flex items-center justify-between">
 					<Logo className="fill-black dark:fill-white" />
