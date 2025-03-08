832a23b19aa45405ec64bbf3084ad90dac05e641
Ayoola Abdulqudus <qqudusayo@gmail.com>
2025-03-08 10:58:30 +0100

    feat: add clicker component and integrate into about overview according to the design

---
+++ src/components/clicker.tsx
@@ -0,0 +1,37 @@
+import { cn } from "../util";
+import { Cursor } from "./icons";
+
+export const Clicker = ({
+	title,
+	fill,
+	className,
+	bottomCursor,
+}: {
+	title: string;
+	fill: string;
+	className?: string;
+	bottomCursor?: boolean;
+}) => (
+	<div className={cn(className)}>
+		<div className="relative">
+			<Cursor
+				className={cn(
+					"absolute -left-4",
+					bottomCursor
+						? "bottom-0 top-7 -rotate-[55deg] transform"
+						: "-top-2.5",
+				)}
+				fill={fill}
+			/>
+			<div
+				className={cn(
+					"w-fit rounded p-2 py-1.5 text-sm text-white",
+					`bg-[${fill}]`,
+					bottomCursor ? "rounded-bl-[1px]" : "rounded-tl-[1px]",
+				)}
+			>
+				{title}
+			</div>
+		</div>
+	</div>
+);
